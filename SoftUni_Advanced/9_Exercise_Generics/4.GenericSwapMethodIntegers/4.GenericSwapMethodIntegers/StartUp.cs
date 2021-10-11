b36ee73e79577fe697a340bdaf0a8b448a74c3e4
PetyrGechev <petyrgechev@gmail.com>
2021-10-11 23:07:40 +0300

    Home Work

---
+++ SoftUni_Advanced/9_Exercise_Generics/4.GenericSwapMethodIntegers/4.GenericSwapMethodIntegers/StartUp.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace _4.GenericSwapMethodIntegers
+{
+
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            var list = new List<int>();
+            for (int i = 0; i < n; i++)
+            {
+                list.Add(int.Parse(Console.ReadLine()));
+            }
+
+            Box<int> box = new Box<int>(list);
+            string[] line = Console.ReadLine().Split();
+            int indexOne = int.Parse(line[0]);
+            int indexTwo = int.Parse(line[1]);
+            box.SwapItems(list, indexOne, indexTwo);
+            Console.WriteLine(box.ToString());
+        }
+    }
+}
+
