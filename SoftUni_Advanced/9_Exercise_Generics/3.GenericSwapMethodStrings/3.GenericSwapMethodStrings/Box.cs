3da0dbb34fd045656aeea759442fd38286ed7d84
PetyrGechev <petyrgechev@gmail.com>
2021-10-11 23:00:32 +0300

    Home Work

---
+++ SoftUni_Advanced/9_Exercise_Generics/3.GenericSwapMethodStrings/3.GenericSwapMethodStrings/Box.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading;
+
+namespace _2.GenericBoxofInteger
+{
+    public class Box<T>
+    {
+        public Box(List<T> elements)
+        {
+            Elements = elements;
+        }
+       
+
+        public List<T> Elements { get;}
+
+        public void SwapItems(List<T> elements, int indexOne, int indexTwo)
+        {
+            T tempElement = elements[indexOne];
+            elements[indexOne] = elements[indexTwo];
+            elements[indexTwo] = tempElement;
+        }
+
+
+        public override string ToString()
+        {
+            var sb = new StringBuilder();
+
+            foreach (var element in Elements)
+            {
+                sb.AppendLine($"{element.GetType()}: {element}");
+            }
+
+            return sb.ToString().TrimEnd();
+            //return $"{Input.GetType()}: {Input}";
+        }
+    }
+}
---
+++ SoftUni_Advanced/9_Exercise_Generics/3.GenericSwapMethodStrings/3.GenericSwapMethodStrings/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace _2.GenericBoxofInteger
+{
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            var list = new List<string>();
+            for (int i = 0; i < n; i++)
+            {
+                list.Add(Console.ReadLine());
+            }
+
+            Box<string> box = new Box<string>(list);
+            string[] line = Console.ReadLine().Split();
+            int indexOne = int.Parse(line[0]);
+            int indexTwo = int.Parse(line[1]);
+            box.SwapItems(list,indexOne,indexTwo);
+            Console.WriteLine(box.ToString());
+        }
+    }
+}
