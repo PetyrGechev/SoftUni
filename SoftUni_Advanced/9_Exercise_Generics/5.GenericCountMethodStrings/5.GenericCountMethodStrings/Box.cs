3006e867fef6d1d6dbf11fb60c146eb66154876a
PetyrGechev <petyrgechev@gmail.com>
2021-10-11 23:50:20 +0300

    Home Work

---
+++ SoftUni_Advanced/9_Exercise_Generics/5.GenericCountMethodStrings/5.GenericCountMethodStrings/Box.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace _5.GenericCountMethodStrings
+{
+    public class Box<T> where T : IComparable
+    {
+        public Box(List<T> elements)
+        {
+            Elements = elements;
+        }
+
+
+        public List<T> Elements { get; }
+
+        public int CompareTo(T elementToCompare)
+        {
+            int counter = 0;
+            foreach (var element in Elements)
+            {
+                if (elementToCompare.CompareTo(element)<0)
+                {
+                    counter++;
+                }
+            }
+            return counter;
+        }
+
+        public void SwapItems(List<T> elements, int indexOne, int indexTwo)
+        {
+            T tempElement = elements[indexOne];
+            elements[indexOne] = elements[indexTwo];
+            elements[indexTwo] = tempElement;
+        }
+
+
+        public override string ToString()
+        {
+            var sb = new StringBuilder();
+
+            foreach (var element in Elements)
+            {
+                sb.AppendLine($"{element.GetType()}: {element}");
+            }
+                
+            return sb.ToString().TrimEnd();
+            //return $"{Input.GetType()}: {Input}";
+        }
+    }
+
+}
---
+++ SoftUni_Advanced/9_Exercise_Generics/5.GenericCountMethodStrings/5.GenericCountMethodStrings/StartUp.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace _5.GenericCountMethodStrings
+{
+  public  class StartUp
+    {
+        static void Main(string[] args)
+        {
+
+            int n = int.Parse(Console.ReadLine());
+            var list = new List<string>();
+            for (int i = 0; i < n; i++)
+            {
+                list.Add(Console.ReadLine());
+            }
+
+            Box<string> box = new Box<string>(list);
+            string elementToCompare = Console.ReadLine();
+            int result = box.CompareTo(elementToCompare);
+            Console.WriteLine(result);
+
+
+
+        }
+    }
+}
