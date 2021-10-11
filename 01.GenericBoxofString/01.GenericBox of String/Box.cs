46ea61f0dd1836b5aac0857e7904073a0a81ae68
PetyrGechev <petyrgechev@gmail.com>
2021-10-11 22:09:24 +0300

    Initial commit

---
+++ 01.GenericBoxofString/01.GenericBox of String/Box.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace _01.GenericBox_of_String
+{
+   public class Box<T>
+    {
+        public Box(T input)
+        {
+            Input = input;
+        }
+        public T Input { get; set; }
+
+
+        public override string ToString()
+        {
+
+            return $"{Input.GetType()}: {Input}";
+        }
+    }
+}
---
+++ 01.GenericBoxofString/01.GenericBox of String/StartUp.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace _01.GenericBox_of_String
+{
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            for (int i = 0; i < n; i++)
+            {
+                
+                Box<string> data = new Box<string>(Console.ReadLine());
+                Console.WriteLine(data.ToString());
+
+            }
+        }
+    }
+}
