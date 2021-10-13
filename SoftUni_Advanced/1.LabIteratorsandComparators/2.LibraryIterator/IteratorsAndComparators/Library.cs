d06b36af6a5c3ec308f812505d5c9ac7b738bc7f
PetyrGechev <petyrgechev@gmail.com>
2021-10-13 20:34:01 +0100

    Home Work

---
+++ SoftUni_Advanced/1.LabIteratorsandComparators/2.LibraryIterator/IteratorsAndComparators/Library.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Text;
+
+namespace IteratorsAndComparators
+{
+    public class Library : IEnumerable
+
+    {
+
+    public List<Book> Books { get; set; }
+
+    public Library(params Book[] books)
+    {
+        Books = new List<Book>(books);
+    }
+
+
+
+
+    private class LibraryIterator : IEnumerator<Book>
+    {
+        private List<Book> books { get; set; }
+        private int currentIndex;
+        public LibraryIterator(List<Book> books)
+        {
+            Reset();
+            this.books = new List<Book>(books);
+        }
+        public Book Current => books[currentIndex];
+
+        object IEnumerator.Current => throw new NotImplementedException();
+
+        public void Dispose()
+        {
+
+        }
+
+        public bool MoveNext()
+        {
+            currentIndex++;
+            if (currentIndex<books.Count)
+            {
+                return true;
+
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        public void Reset()
+        {
+            currentIndex = -1;
+        }
+    }
+
+        public IEnumerator<Book> GetEnumerator()
+        {
+            return new LibraryIterator(Books);
+        }
+
+        IEnumerator IEnumerable.GetEnumerator() => this.GetEnumerator();
+
+    }
+}
---
+++ SoftUni_Advanced/1.LabIteratorsandComparators/IteratorsAndComparators/IteratorsAndComparators/Library.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Text;
+
+namespace IteratorsAndComparators
+{
+    public class Library : IEnumerable
+
+    {
+
+    public List<Book> Books { get; set; }
+
+    public Library(params Book[] books)
+    {
+        Books = new List<Book>(books);
+    }
+
+        public IEnumerator GetEnumerator()
+        {
+            foreach (var book in Books)
+            {
+                yield return book.Title;
+            }
+        }
+    }
+}
