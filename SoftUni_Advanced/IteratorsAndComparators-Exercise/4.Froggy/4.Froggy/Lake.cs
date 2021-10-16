b3def3510fa58d094ce71a3ef8dc9e674eb8b8ab
PetyrGechev <petyrgechev@gmail.com>
2021-10-16 14:02:59 +0100

    Home Work

---
+++ SoftUni_Advanced/IteratorsAndComparators-Exercise/4.Froggy/4.Froggy/Lake.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Text;
+
+namespace _4.Froggy
+{
+    public class Lake<T> : IEnumerable<int>
+    {
+        public Lake(params int[] stones)
+        {
+            this.stones = stones;
+        }
+
+        private int[] stones;
+
+
+        public IEnumerator<int> GetEnumerator()
+        {
+            List<int> result = new List<int>();
+            for (int i = 0; i < stones.Length; i++)
+            {
+                if (i % 2 == 0)
+                {
+                    yield return stones[i];
+
+                }
+            }
+
+            for (int i = stones.Length - 1; i >= 0; i--)
+            {
+                if (i % 2 != 0)
+                {
+                    yield return stones[i];
+
+                }
+            }
+        }
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return GetEnumerator();
+        }
+    }
+}
---
+++ SoftUni_Advanced/IteratorsAndComparators-Exercise/4.Froggy/4.Froggy/StartUp.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Linq;
+
+namespace _4.Froggy
+{
+    class StartUp
+    {
+        public static void Main(string[] args)
+        {
+            Lake<int> lake = new Lake<int>(Console.ReadLine().Split(", ").Select(int.Parse).ToArray());
+            Console.WriteLine(string.Join(", ",lake));
+        }
+    }
+}
