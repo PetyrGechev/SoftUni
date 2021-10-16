7000a0abc95c341b08820e9042bebd1366c4ae44
PetyrGechev <petyrgechev@gmail.com>
2021-10-16 13:26:58 +0100

    Home Work

---
+++ SoftUni_Advanced/IteratorsAndComparators-Exercise/PrivateStack/PrivateStack/Stack.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace PrivateStack
+{
+    public class Stack<T> :IEnumerable<T>
+    {
+        private int index;
+        private List<T> collection;
+
+        public Stack(params T[] data)
+        {
+            collection = new List<T>(data);
+            index = 0;
+        }
+
+        public void Push(params T[] elements)
+        {
+            collection.AddRange(elements);
+
+        }
+
+        public string Pop()
+        {
+            
+            if (!collection.Any())
+            {
+                 return ($"No elements");
+                  
+
+
+            }
+            string element = collection.LastOrDefault().ToString();
+            collection.RemoveAt(collection.Count-1);
+            return element;
+        }
+
+        public IEnumerator<T> GetEnumerator()
+        {
+            for (int i = collection.Count - 1; i >= 0; i--)
+            {
+                yield return collection[i];
+            }
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return GetEnumerator();
+        }
+    }
+}
---
+++ SoftUni_Advanced/IteratorsAndComparators-Exercise/PrivateStack/PrivateStack/StartUp.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Linq;
+
+namespace PrivateStack
+{
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+            string command = "";
+            Stack<string> Tstack = new Stack<string>();
+            while ((command=Console.ReadLine())!="END")
+            {
+                if (command.Contains("Push"))
+                {
+                    command = command.Remove(0, 5);
+
+                    string[] commands = command.Split(", ");
+
+
+                    Tstack.Push(commands);
+                }
+                else
+                {
+                    string element = Tstack.Pop();
+                    if (element== "No elements")
+                    {
+                        Console.WriteLine("No elements");
+                        continue;
+                    }
+
+                    
+                }
+            }
+
+            foreach (var item in Tstack)
+            {
+                Console.WriteLine(item);
+            }
+            foreach (var item in Tstack)
+            {
+                Console.WriteLine(item);
+            }
+        }
+    }
+}
