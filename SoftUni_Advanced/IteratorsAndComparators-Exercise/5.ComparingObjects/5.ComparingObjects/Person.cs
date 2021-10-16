f4db29b18a59954efaccdfee9cb0c4fcef53d2e9
PetyrGechev <petyrgechev@gmail.com>
2021-10-16 14:29:37 +0100

    Home Work

---
+++ SoftUni_Advanced/IteratorsAndComparators-Exercise/5.ComparingObjects/5.ComparingObjects/Person.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
+using System.Text;
+
+namespace _5.ComparingObjects
+{
+    public class Person : IComparable<Person>
+    {
+        // Peter 22 Varna
+
+        public Person(string name, int age, string city)
+        {
+            Name = name;
+            Age = age;
+            City = city;
+        }
+
+        public string Name { get; set; }
+        public int Age { get; set; }
+        public string City { get; set; }
+
+        public int CompareTo(Person other)
+        {
+            if (this.Name.CompareTo(other.Name) == 0 && this.Age.CompareTo(other.Age) == 0&& this.City.CompareTo(other.City) == 0)
+            {
+                return 1;
+            }
+
+            return -1;
+
+
+            
+        }
+    }
+}
---
+++ SoftUni_Advanced/IteratorsAndComparators-Exercise/5.ComparingObjects/5.ComparingObjects/StartUp.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace _5.ComparingObjects
+{
+    class StartUp
+    {
+        public static void Main(string[] args)
+        {
+            // Peter 22 Varna
+             int equalPeople = -1;
+             int notEqualPeople = -1;
+        List<Person> people = new List<Person>();
+            string command = "";
+            while ((command=Console.ReadLine())!="END")
+            {
+                string[] info = command.Split();
+                string name = info[0];
+                int age = int.Parse(info[1]);
+                string city = info[2];
+                Person person = new Person(name, age, city);
+                people.Add(person);
+            }
+
+            int indexOfPerson = int.Parse(Console.ReadLine())-1;
+
+            foreach (var person in people)
+            {
+                if (people[indexOfPerson].CompareTo(person)==1)
+                {
+                    equalPeople++;
+                }
+                else
+                {
+                    notEqualPeople++;
+                }
+            }
+
+            Console.WriteLine(equalPeople > 0 ? $"{equalPeople+1} {notEqualPeople+1} {people.Count}" : "No matches");
+        }
+    }
+}
