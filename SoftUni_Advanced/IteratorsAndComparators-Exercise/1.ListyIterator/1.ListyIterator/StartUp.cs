b378a836d3f5747724be57ad2ba1d2a8338311a2
PetyrGechev <petyrgechev@gmail.com>
2021-10-16 12:44:27 +0100

    Home Work

---
+++ SoftUni_Advanced/IteratorsAndComparators-Exercise/1.ListyIterator/1.ListyIterator/StartUp.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.ComponentModel.Design;
+using System.Linq;
+
+namespace _1.ListyIterator
+{
+    class StartUp
+    {
+        static void Main(string[] args)
+        {
+            string command ="";
+            ListyIterator<string> listy = null;
+            while ((command=Console.ReadLine()) != "END")
+            {
+                
+                string[] commands = command.Split();
+                if (commands[0]=="Create")
+                {
+                    listy = new ListyIterator<string>(commands.Skip(1).ToArray());
+                }
+                else if (commands[0] == "Print")
+                {
+                    listy.Print();
+                    
+                }
+                else if (commands[0] == "Move")
+                {
+                    Console.WriteLine(listy.Move());
+                }
+                else if (commands[0] == "HasNext")
+                {   
+
+                    Console.WriteLine(listy.HasNext());
+                }
+                else if (commands[0] == "PrintAll")
+                {
+
+                    listy.PrintAll();
+                }
+
+
+            }
+
+        }
+    }
+}
---
+++ SoftUni_Advanced/IteratorsAndComparators-Exercise/1.ListyIterator/2.ListyIterator/ListyIterator.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Channels;
+
+namespace _1.ListyIterator
+{
+    public class ListyIterator<T>
+    {
+        public ListyIterator(params T[] list)
+        {
+            this.list = new List<T>(list);
+            index = 0;
+        }
+
+        private List<T> list;
+        private int index ;
+      
+
+        public bool HasNext()
+        {
+            if (index < list.Count - 1)
+            {
+                return true;
+            }
+
+            return false;
+
+        }
+
+        public bool Move()
+        {
+            if (HasNext())
+            {
+                index++;
+                return true;
+            }
+
+
+
+            return false;
+        }
+
+        public void Print()
+        {
+            if (list.Count == 0)
+            {
+                throw new ArgumentException("Invalid Operation!");
+            }
+
+            Console.WriteLine(list[index]);
+
+        }
+
+    }
+}
