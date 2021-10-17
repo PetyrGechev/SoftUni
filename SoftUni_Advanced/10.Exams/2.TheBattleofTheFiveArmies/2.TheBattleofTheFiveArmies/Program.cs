f46e26d79e9a492c8fc57d2356219fbca3b8c9bc
PetyrGechev <petyrgechev@gmail.com>
2021-10-17 13:29:59 +0100

    Home Work

--- SoftUni_Advanced/10.Exams/2.TheBattleofTheFiveArmies/2.TheBattleofTheFiveArmies/Program.cs
+++ SoftUni_Advanced/10.Exams/2.TheBattleofTheFiveArmies/2.TheBattleofTheFiveArmies/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
 
 namespace _2.TheBattleofTheFiveArmies
 {
@@ -6,7 +8,125 @@ namespace _2.TheBattleofTheFiveArmies
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            int armor = int.Parse(Console.ReadLine());
+            int numberOfRows = int.Parse(Console.ReadLine());
+            char[][] matrix = new char[numberOfRows][];
+            int armyRow = -1;
+            int armyCol = -1;   
+           
+
+            for (int row = 0; row < numberOfRows; row++)
+            {
+                char[] input = Console.ReadLine().ToCharArray();
+                matrix[row] = input;
+
+            }
+
+            for (int row = 0; row < matrix.Length; row++)
+            {
+                for (int col = 0; col < matrix[row].Length; col++)
+                {
+                    if (matrix[row][col] == 'A')
+                    {
+                        armyRow = row;
+                        armyCol = col;
+                    }
+                }
+            }
+            while (true)
+            {
+                if (armor <= 0)
+                {
+
+                    matrix[armyRow][armyCol] = 'X';
+
+                    Console.WriteLine($"The army was defeated at {armyRow};{armyCol}.");
+                    PrintMatrix(matrix);
+                    break;
+
+
+                }
+                //up 3 0
+                string[] input = Console.ReadLine().Split();
+                string direction = input[0];
+                int OrcRow = int.Parse(input[1]);
+                int OrcCol = int.Parse(input[2]);
+                matrix[OrcRow][OrcCol] = 'O';
+                matrix[armyRow][armyCol] = '-';
+                if (direction == "up" && IsInsideMatrix(matrix, armyRow - 1, armyCol))
+                {
+
+                    armyRow = armyRow - 1;
+
+
+                }
+                else if (direction == "down" && IsInsideMatrix(matrix, armyRow + 1, armyCol))
+                {
+                    armyRow = armyRow + 1;
+
+                }
+                else if (direction == "right" && IsInsideMatrix(matrix, armyRow, armyCol + 1))
+                {
+                    armyCol = armyCol + 1;
+
+                }
+                else if (direction == "left" && IsInsideMatrix(matrix, armyRow, armyCol - 1))
+                {
+                    armyCol = armyCol - 1;
+                }
+                armor -= 1;
+
+
+                if (matrix[armyRow][armyCol] == 'O')
+                {
+                    armor -= 2;
+                    if (armor <= 0)
+                    {
+
+                        matrix[armyRow][armyCol] = 'X';
+                       
+                        Console.WriteLine($"The army was defeated at {armyRow};{armyCol}.");
+                        PrintMatrix(matrix);
+                        break;
+
+
+                    }
+
+                }
+                else if (matrix[armyRow][armyCol] == 'M')
+                {
+                    Console.WriteLine($"The army managed to free the Middle World! Armor left: {armor}");
+                    matrix[armyRow][armyCol] = '-';
+                    PrintMatrix(matrix);
+
+                    break;
+                }
+                matrix[armyRow][armyCol] = 'A';
+                PrintMatrix(matrix);
+            }
+
+        }
+
+        private static bool IsInsideMatrix(char[][] matrix, int armyRow, int armyCol)
+        {
+            if (armyRow < 0 || armyRow >= matrix.Length || armyCol < 0 || armyCol >= matrix[armyRow].Length)
+            {
+                return false;
+            }
+
+            return true;
+        }
+
+        public static void PrintMatrix(char[][] matrix)
+        {
+            StringBuilder sb = new StringBuilder();
+            foreach (var row in matrix)
+            {
+                string output = string.Join("", row);
+                sb.AppendLine(output);
+            }
+
+            Console.WriteLine(sb.ToString().TrimEnd());
         }
     }
 }
