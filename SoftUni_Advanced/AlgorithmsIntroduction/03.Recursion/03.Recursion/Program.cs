a1daefb02037755437e08d0530c9896b42d41b1b
PetyrGechev <petyrgechev@gmail.com>
2021-10-24 11:56:42 +0100

    Home work

--- SoftUni_Advanced/AlgorithmsIntroduction/03.Recursion/03.Recursion/Program.cs
+++ SoftUni_Advanced/AlgorithmsIntroduction/03.Recursion/03.Recursion/Program.cs
@@ -6,7 +6,19 @@ namespace _03.Recursion
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            int[] numbers = new int[] {2, 3, 4, 5};
+            Console.WriteLine(SumArray(numbers,0));
+            static int  SumArray(int[] arrey,int startIndex)
+            {
+
+                if (startIndex==arrey.Length)
+                {
+                    return 0;
+                }
+
+                return arrey[startIndex] + SumArray(arrey, startIndex + 1);
+            }
+
         }
     }
 }
