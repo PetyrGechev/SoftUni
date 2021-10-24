01f950b0f8634378aa5d65a99fcd1ab69d9a8e4d
PetyrGechev <petyrgechev@gmail.com>
2021-10-24 11:26:17 +0100

    Home work

---
+++ SoftUni_Advanced/AlgorithmsIntroduction/01.Recursion/01.Recursion/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace _01.Recursion
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Print(10);
+            static void Print(int n )
+            {
+                if (n==-1)
+                {
+                    return ;
+                }
+                Console.WriteLine(n);
+                Print(n-1);
+                Console.WriteLine(n);
+            }
+        }
+    }
+}
