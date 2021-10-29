fd8f71649edc878f73ab2df69701d785099cc4ab
PetyrGechev <petyrgechev@gmail.com>
2021-10-29 19:48:38 +0100

    Home work

--- SoftUni_OPP/Inheritance_Exercise/PlayersAndMonsters/StartUp.cs
+++ SoftUni_OPP/Inheritance_Exercise/PlayersAndMonsters/StartUp.cs
@@ -1,10 +1,13 @@
-﻿namespace PlayersAndMonsters
+﻿using System;
+
+namespace PlayersAndMonsters
 {
     public class StartUp
     {
         public static void Main(string[] args)
         {
-
+            Knight knight = new Knight("pesho", 2);
+            Console.WriteLine(knight.ToString());
         }
     }
 }
