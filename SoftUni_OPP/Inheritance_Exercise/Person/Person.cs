c614becdad2f004c957ca99aeb69230127dcb804
PetyrGechev <petyrgechev@gmail.com>
2021-10-29 18:21:37 +0100

    Home work

---
+++ SoftUni_OPP/Inheritance_Exercise/Person/Person.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Person
+{
+   public  class Person
+   {
+       private string name;
+       private int age;
+       public Person(string name, int age)
+       {
+           this.name = name;
+           this.age = age;
+       }
+       // public string Name { get; set; }
+       //public int Age { get; set; }
+       public override string ToString()
+       {
+           return $"Name: {name}, Age: {age} ";
+       }
+   }
+}
