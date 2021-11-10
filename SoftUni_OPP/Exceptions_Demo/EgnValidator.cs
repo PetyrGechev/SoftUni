b721134fd9d1590c233d84dd736b116c9e973f3c
PetyrGechev <petyrgechev@gmail.com>
2021-11-10 21:29:10 +0000

    Home work

---
+++ SoftUni_OPP/Exceptions_Demo/EgnValidator.cs
@@ -0,0 +1,64 @@
+﻿using System;
+
+namespace ExceptionsDemos
+{
+    public enum Gender
+    {
+        Male = 1,
+        Female = 2,
+    }
+
+    public class EgnValidator : IEgnValidator
+    {
+        // TODO: dictionary of cities
+
+        /// <summary>
+        /// Generate all valid EGN numbers for given criteria.
+        /// </summary>
+        /// <param name="birthDate">Date of birth.</param>
+        /// <param name="city">The city where EGN holders are born in.</param>
+        /// <param name="isMale">True for male, false for female</param>
+        /// <returns>List of all valid EGN numbers</returns>
+        /// <exception cref="System.ArgumentException"></exception>
+        /// <exception cref="System.ArgumentNullException"></exception>
+        /// <exception cref="System.ArgumentOutOfRangeException"></exception>
+        /// <exception cref="InvalidCityException"></exception>
+        public string[] Generate(DateTime birthDate, string city, bool isMale)
+        {
+            if (birthDate.Year < 1800) // [1800-2099]
+            {
+                throw new ArgumentOutOfRangeException("birthDate", "Birth date should be greater or equal to 1800");
+            }
+
+            if (birthDate.Year > 2099) // [1800-2099]
+            {
+                throw new ArgumentOutOfRangeException("birthDate", "Birth date should be less or equal to 2099");
+            }
+
+            if (city == null)
+            {
+                throw new ArgumentNullException("birthDate");
+            }
+
+            if (city != "София-град")
+            {
+                throw new InvalidCityException(city);
+            }
+
+            // TODO: Add more checks and exception
+
+            return new string[0];
+        }
+
+        
+        public bool Validate(string egn)
+        {
+            if (egn == null)
+            {
+                throw new ArgumentNullException("egn");
+            }
+
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ SoftUni_OPP/Exceptions_Demo/Program.cs
@@ -0,0 +1,133 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+
+namespace ExceptionsDemos
+{
+
+    class Program
+    {
+        static void Main()
+        {
+            int num = 1;
+            for (int i = 0; i < 0; i++)
+            {
+                num *= 2;
+            }
+
+            Console.WriteLine(num);
+
+
+            var a = 2;
+            new int[0].Where(x => a++ != 0);
+
+            Console.OutputEncoding = Encoding.Unicode;
+            A();
+
+            IEgnValidator validator = new EgnValidator();
+            try
+            {
+                validator.Validate(null);
+            }
+            catch (ArgumentNullException)
+            {
+                Console.WriteLine("EGN should not be empty!");
+            }
+            catch (Exception)
+            {
+
+            }
+
+            try
+            {
+                // TODO: Read from user
+                validator.Generate(new DateTime(1907, 1, 1), "Калифорния", false);
+            }
+            catch (InvalidCityException ex)
+            {
+                Console.WriteLine("Invalid city:" + ex.CityName);
+            }
+
+            Console.WriteLine(ReadInteger() * 10);
+            // ReadInteger();
+            // int number = int.Parse("милион и едно");
+
+
+            Console.WriteLine("End of Main()");
+        }
+
+        public static void A()
+        {
+            try
+            {
+                B();
+            }
+            catch (Exception ex)
+                when (ex is ArgumentNullException || ex is ArgumentOutOfRangeException)
+            {
+                Console.WriteLine("ERROR IN B();");
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("ERROR IN B(); general");
+            }
+        }
+
+        public static void B()
+        {
+            C();
+        }
+
+        public static void C()
+        {
+            throw new Exception("..");
+        }
+
+        public static void WriteToFile()
+        {
+            StreamReader sr = null;
+            try
+            {
+                sr = new StreamReader("test.txt");
+                Console.WriteLine(sr.ReadLine());
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("ERROR: " + ex.Message);
+                throw;
+            }
+            finally
+            {
+                sr?.Dispose();
+            }
+
+            Console.WriteLine();
+        }
+
+        public static int ReadInteger()
+        {
+            while (true)
+            {
+                try
+                {
+                    string line = Console.ReadLine();
+                    return int.Parse(line);
+                }
+                catch (FormatException ex)
+                {
+                    Console.WriteLine($"Invalid number format ({ex.Message}).");
+                }
+                catch (OverflowException)
+                {
+                    Console.WriteLine("Please enter number less than " + int.MaxValue);
+                }
+                finally
+                {
+                    Console.WriteLine("FINALLY!!!");
+                }
+            }
+        }
+    }
+}
