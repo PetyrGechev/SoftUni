78e7409d4955f26fa08257596dd2ab4370fdb6d3
PetyrGechev <petyrgechev@gmail.com>
2021-11-23 21:58:45 +0000

    Home work

--- SoftUni_OPP/UnitTesting_Exercise/DatabaseExtended.Tests/ExtendedDatabase.Tests.cs
+++ SoftUni_OPP/UnitTesting_Exercise/DatabaseExtended.Tests/ExtendedDatabase.Tests.cs
@@ -1,4 +1,4 @@
-using ExtendedDatabase;
+using System;
 using NUnit.Framework;
 
 
@@ -7,11 +7,11 @@ namespace Tests
 {
     public class ExtendedDatabaseTests
     {
-        private ExtendedDatabase.ExtendedDatabase extendedDatabase;
+        private ExtendedDatabase extendedDatabase;
         [SetUp]
         public void Setup()
         {
-            extendedDatabase = new ExtendedDatabase.ExtendedDatabase();
+            extendedDatabase = new ExtendedDatabase();
         }
 
         [Test]
@@ -24,13 +24,174 @@ namespace Tests
 
             }
 
-            extendedDatabase = new ExtendedDatabase.ExtendedDatabase(people);
+            extendedDatabase = new ExtendedDatabase(people);
             Assert.That(extendedDatabase.Count,Is.EqualTo(people.Length));
             foreach (var person in people)
             {
                 Person personFromDb = extendedDatabase.FindById(person.Id);
-                Assert.That(person, Is.EqualTo(personFromDb));                 /////////  ! ! ! !
+                Assert.That(person, Is.EqualTo(personFromDb));                 
             }
+        }
+        [Test]
+        public void Ctor_ThrowsExceptionWhenCapacityIsExceeded()
+        {
+            var people = new Person[17];
+            for (int i = 0; i < people.Length; i++)
+            {
+                people[i] = new Person(i, $"Name_{i}");
+
+            }
+
+            Assert.Throws<ArgumentException>(() => extendedDatabase = new ExtendedDatabase(people));
+        }
+        [Test]
+        public void Add_ShoudNotAddMoreThan16Elements()
+        {
+            var people = new Person[16];
+            for (int i = 0; i < people.Length; i++)
+            {
+                people[i] = new Person(i, $"Name_{i}");
+
+            }
+
+            extendedDatabase = new ExtendedDatabase(people);
+            Assert.Throws<InvalidOperationException>(() => extendedDatabase.Add(new Person(16,"Name_16")));
+        }
+        [Test]
+        public void Add_ShoudNotAddPersonWithSameID()
+        {
+            Person personOne = new Person(1, "Gogi");
+            Person personTwo = new Person(1, "Babun");
+            
+            extendedDatabase = new ExtendedDatabase(personOne);
+
+            Assert.Throws<InvalidOperationException>(() => extendedDatabase.Add(personTwo));
+        }
+        [Test]
+        public void Add_ShoudNotAddPersonWithSameName()
+        {
+            Person personOne = new Person(1, "Gogi");
+            Person personTwo = new Person(12, "Gogi");
+
+            extendedDatabase = new ExtendedDatabase(personOne);
+
+            Assert.Throws<InvalidOperationException>(() => extendedDatabase.Add(personTwo));
+        }
+        [Test]
+        public void Add_ShoudAddPersonToDB()
+        {
+            Person personOne = new Person(1, "Gogi");
+          
+
+            extendedDatabase = new ExtendedDatabase();
+            extendedDatabase.Add(personOne);
+
+            Assert.That(extendedDatabase.Count,Is.EqualTo(1));
+        }
+
+        [Test]
+        public void Remove_ThrowsExceptionWhenDBIsEmpty()
+        {
+            
+            extendedDatabase = new ExtendedDatabase();
+            Assert.Throws<InvalidOperationException>(() => extendedDatabase.Remove());
+
+        }
+        [Test]
+        public void Remove_ShoudRemoveItemFromDB()
+        {
+
+            Person personOne = new Person(1, "Gogi");
+            Person personTwo = new Person(2, "Kiro");
+
+
+            extendedDatabase = new ExtendedDatabase();
+            extendedDatabase.Add(personOne);
+            extendedDatabase.Add(personTwo);
+            extendedDatabase.Remove();
+
+            Assert.That(extendedDatabase.Count, Is.EqualTo(1));
+            Person person = extendedDatabase.FindByUsername("Gogi");
+            Assert.That(person,Is.EqualTo(personOne));
+
+        }
+        [TestCase("")]
+        [TestCase(null)]
+
+        public void FindByUsername_ThrowsExpectionWhenNameisEmptyOrNull(string name)
+        {
+
+            Person personOne = new Person(1, "Gogi");
+            
+            extendedDatabase = new ExtendedDatabase();
+            extendedDatabase.Add(personOne);
+            Assert.Throws<ArgumentNullException>(() => extendedDatabase.FindByUsername(name));
+
+        }
+        [TestCase("Koko")]
+        [TestCase("Gigi")]
+
+        public void FindByUsername_ThrowsExpectionWhenCantFindTheName(string name)
+        {
+
+            Person personOne = new Person(1, "Gogi");
+
+            extendedDatabase = new ExtendedDatabase();
+            extendedDatabase.Add(personOne);
+            Assert.Throws<InvalidOperationException>(() => extendedDatabase.FindByUsername(name));
+
+        }
+        [Test]
+        public void FindByUsername_ShoudFindTheRightPersonByName()
+        {
+
+            Person personOne = new Person(1, "Gogi");
+
+            extendedDatabase = new ExtendedDatabase();
+            extendedDatabase.Add(personOne);
+            Person personFromDb = extendedDatabase.FindByUsername("Gogi");
+            Assert.That(personOne,Is.EqualTo(personFromDb));
+
+        }
+        [Test]
+        public void FindByID_ShoudFindTheRightPersonByID()
+        {
+
+            Person personOne = new Person(1, "Gogi");
+            Person personTwo = new Person(2, "Kiro");
+
+            extendedDatabase = new ExtendedDatabase();
+            extendedDatabase.Add(personOne);
+            extendedDatabase.Add(personTwo);
+            Person personFromDb = extendedDatabase.FindById(1);
+            Assert.That(personOne, Is.EqualTo(personFromDb));
+
+        }
+        [Test]
+        public void FindByID_ThrowsExpectionWhenIDisNegative()
+        {
+
+            Person personOne = new Person(1, "Gogi");
+            Person personTwo = new Person(2, "Kiro");
+
+            extendedDatabase = new ExtendedDatabase();
+            extendedDatabase.Add(personOne);
+            extendedDatabase.Add(personTwo);
+            Assert.Throws<ArgumentOutOfRangeException>(() => extendedDatabase.FindById(-1));
+
+        }
+        [Test]
+        public void FindByID_ThrowsExpectionWhenCantFindThePerson()
+        {
+
+            Person personOne = new Person(1, "Gogi");
+            Person personTwo = new Person(2, "Kiro");
+
+            extendedDatabase = new ExtendedDatabase();
+            extendedDatabase.Add(personOne);
+            extendedDatabase.Add(personTwo);
+            Assert.Throws<InvalidOperationException>(() => extendedDatabase.FindById(3));
+
         }
 
     }
