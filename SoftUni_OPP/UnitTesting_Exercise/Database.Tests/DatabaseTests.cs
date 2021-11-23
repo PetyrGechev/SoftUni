4602ad6f4ed77a1d3624d359cae58800fcc65a07
PetyrGechev <petyrgechev@gmail.com>
2021-11-23 20:09:36 +0000

    Home work

--- SoftUni_OPP/UnitTesting_Exercise/Database.Tests/DatabaseTests.cs
+++ SoftUni_OPP/UnitTesting_Exercise/Database.Tests/DatabaseTests.cs
@@ -1,4 +1,6 @@
 using NUnit.Framework;
+using System;
+using System.Linq;
 
 namespace Tests
 {
@@ -10,9 +12,128 @@ namespace Tests
         }
 
         [Test]
-        public void Test1()
+        [TestCase(3)]
+        [TestCase(12)]
+        [TestCase(16)]
+        [TestCase(0)]
+        public void AddMethodShouldAddNewElementsWhileCountIsLessThan16(int count)
         {
-            Assert.Pass();
+            //Arrange
+            Database database = new Database();
+
+            //Act
+            for (int i = 0; i < count; i++)
+            {
+                database.Add(i);
+            }
+
+            //Assert
+            Assert.AreEqual(count, database.Count);
+
+        }
+
+        [Test]
+        public void AddMethodShouldThrowExceptionWhenItemsAreAbove16()
+        {
+            //Arrange
+            Database database = new Database();
+
+            //Act
+            for (int i = 0; i < 16; i++)
+            {
+                database.Add(i);
+            }
+
+            //Assert
+            Assert.Throws<InvalidOperationException>(() => database.Add(5), "Array's capacity must be exactly 16 integers!");
+        }
+
+        [Test]
+        [TestCase(1, 4)]
+        [TestCase(1, 15)]
+        [TestCase(1, 16)]
+        public void ConstructorShouldAllItemsWhileTheyAreBelow16(
+            int start,
+            int count)
+        {
+            //Arrange
+            int[] elements = Enumerable.Range(start, count).ToArray();
+
+            //Act
+            Database database = new Database(elements);
+
+            //Assert
+            Assert.AreEqual(count, database.Count);
+        }
+
+        [Test]
+        [TestCase(1, 17)]
+        [TestCase(1, 25)]
+        public void ConstructorShouldThrowExceptionWhenItemsAreAbove16(
+            int start,
+            int count)
+        {
+            //Arrange
+            int[] elements = Enumerable.Range(start, count).ToArray();
+
+            //Act
+            Assert.Throws<InvalidOperationException>(()
+                => new Database(elements));
+        }
+
+        [Test]
+        [TestCase(1, 10, 3, 7)]
+        [TestCase(1, 5, 4, 1)]
+        public void RemoveMethodShoudlRemoveElementsWhenTheyAreAbove0(
+            int start,
+            int count,
+            int toRemove,
+            int result)
+        {
+            //Arrange
+            int[] elements = Enumerable.Range(start, count).ToArray();
+            Database database = new Database(elements);
+
+            //Act
+            for (int i = 0; i < toRemove; i++)
+            {
+                database.Remove();
+            }
+
+            //Assert
+            Assert.AreEqual(result, database.Count);
+        }
+
+        [Test]
+        public void RemoveMethodShouldThrowExceptionWhenElementsAre0()
+        {
+            //Arrange
+            Database database = new Database();
+
+            //Act
+            Assert.Throws<InvalidOperationException>(
+                () => database.Remove());
+        }
+
+        [Test]
+        public void FetchShouldReturnAllValidItems()
+        {
+            //Arrange
+            Database database = new Database(1, 2, 3);
+
+            //Act
+            database.Add(4);
+            database.Add(5);
+
+            database.Remove();
+            database.Remove();
+            database.Remove();
+
+            int[] fetchedData = database.Fetch();
+
+            //Assert
+            int[] expectedData = new int[] { 1, 2 };
+            Assert.AreEqual(expectedData, fetchedData);
         }
     }
 }
