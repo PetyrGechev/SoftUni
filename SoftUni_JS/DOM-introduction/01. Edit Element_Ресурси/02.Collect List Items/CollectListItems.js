1aa800a30c2206107472787475bdbd79d574d6c9
Petar Milkov Gechev <92267207+PetyrGechev@users.noreply.github.com>
2022-01-18 19:23:00 +0000

    Update CollectListItems.js

--- SoftUni_JS/DOM-introduction/01. Edit Element_Ресурси/02.Collect List Items/CollectListItems.js
+++ SoftUni_JS/DOM-introduction/01. Edit Element_Ресурси/02.Collect List Items/CollectListItems.js
@@ -1,3 +1,12 @@
 function extractText() {
-    // TODO
+    let result=[]
+    const text=document.getElementById('items').children;
+    let textarea=document.getElementById('result');
+    for (let index = 0; index < text.length; index++) {
+    
+        result.push(text[index].textContent);
+    }
+   console.log(result)
+   textarea.value=result.join("\n")
+
 }
