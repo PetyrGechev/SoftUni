9b9d59450c9bf5f1f48098442f19a5fd288d8358
Petar Milkov Gechev <92267207+PetyrGechev@users.noreply.github.com>
2022-01-23 13:55:33 +0000

    Update search.js

--- SoftUni_JS/DOM-Introduction-Exercise-Resources-New/04. Search in List/search.js
+++ SoftUni_JS/DOM-Introduction-Exercise-Resources-New/04. Search in List/search.js
@@ -1,3 +1,25 @@
 function search() {
-   // TODO
+   let towns=Array.from(document.querySelectorAll("ul li"));
+   let result=document.getElementById("result");
+   let search=document.getElementById("searchText").value;
+   let matches=0;
+   towns.forEach(element => {
+     let town=element.textContent;
+
+      if (town.includes(search)) {
+         matches++;
+         element.style.fontWeight="bold";
+         element.style.textDecoration = "underline";
+      }else{
+         element.style.fontWeight="normal";
+         element.style.textDecoration = "none";
+      }
+
+   });
+   if(matches!=0){
+      result.textContent=`${matches} matches found`;
+   }else{
+      result.textContent='';
+   }
+   
 }
