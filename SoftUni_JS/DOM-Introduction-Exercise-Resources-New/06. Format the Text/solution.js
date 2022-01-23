8c39423951c8937cc37e6b3639e2ad7ccfb15574
Petar Milkov Gechev <92267207+PetyrGechev@users.noreply.github.com>
2022-01-23 18:01:53 +0000

    Update solution.js

--- SoftUni_JS/DOM-Introduction-Exercise-Resources-New/06. Format the Text/solution.js
+++ SoftUni_JS/DOM-Introduction-Exercise-Resources-New/06. Format the Text/solution.js
@@ -1,3 +1,23 @@
 function solve() {
-  //TODO
+  //(`<p> {text} </p>`)
+  let outText=document.getElementById("output");
+  let input=document.getElementById("input").value.split(".").filter((p) => p.length > 0);;
+  let result='';
+  let temp='';
+  console.log(input)
+  for (let i = 1; i <= input.length; i++) {
+    
+    temp+=input[i-1]+'.';
+    if(i%3==0 ){
+      result+=`<p> ${temp} </p> `
+      temp='';
+    }
+    if(i==input.length){
+      result+=`<p> ${temp} </p> `
+    }
+   
+    
+  }
+  console.log(result)
+  outText.innerHTML=result
 }
