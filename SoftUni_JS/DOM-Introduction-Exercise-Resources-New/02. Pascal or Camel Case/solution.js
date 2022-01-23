ddbd8cac40b9e1971af0c11c6d66bca96a90b88a
Petar Milkov Gechev <92267207+PetyrGechev@users.noreply.github.com>
2022-01-23 13:02:50 +0000

    Update solution.js

--- SoftUni_JS/DOM-Introduction-Exercise-Resources-New/02. Pascal or Camel Case/solution.js
+++ SoftUni_JS/DOM-Introduction-Exercise-Resources-New/02. Pascal or Camel Case/solution.js
@@ -1,3 +1,33 @@
 function solve() {
-  //TODO...
+    let input=document.getElementById("text").value ;
+    let result='';
+    let splitedText=input.split(' ');
+    let namingConv=document.getElementById("naming-convention").value;
+  
+    if(namingConv=="Camel Case"){
+      let firstword=splitedText[0].toLowerCase()
+      result+=firstword
+      for (let index = 1; index < splitedText.length; index++) {
+        let word=splitedText[index].toLowerCase();
+        word=word[0].toUpperCase()+word.slice(1);
+        result+=word;
+        
+      }
+
+    }else if(namingConv=="Pascal Case"){
+      for (let index = 0; index < splitedText.length; index++) {
+        let word=splitedText[index].toLowerCase();
+        word=word[0].toUpperCase()+word.slice(1);
+        result+=word;
+        
+      }
+
+    }else{
+      let finalResult=document.getElementById("result");
+      finalResult.textContent="Error!";
+      return;
+      
+    }
+    let finalResult=document.getElementById("result");
+    finalResult.textContent=result;
 }
