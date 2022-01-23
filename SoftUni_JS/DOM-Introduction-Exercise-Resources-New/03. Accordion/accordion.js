f1ce7915fae1146fe606c62f3239aed36d58ea2d
Petar Milkov Gechev <92267207+PetyrGechev@users.noreply.github.com>
2022-01-23 13:28:13 +0000

    Home work

--- SoftUni_JS/DOM-Introduction-Exercise-Resources-New/03. Accordion/accordion.js
+++ SoftUni_JS/DOM-Introduction-Exercise-Resources-New/03. Accordion/accordion.js
@@ -1,3 +1,16 @@
 function toggle() {
-    console.log('TODO:...');
+    let buttonText=document.getElementsByClassName("button")[0].textContent ; 
+    let button=document.getElementsByClassName("button")[0];
+    let extra=document.getElementById("extra");
+   
+    if(buttonText=="More"){
+        button.textContent='Less'
+        
+        let extra=document.getElementById("extra");
+        extra.style.display='block';
+
+    }else{
+        button.textContent='More'
+        extra.style.display='none';
+    }
 }
