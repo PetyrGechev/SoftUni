93db4592772c927effed5549b70f24a519ea79d2
Petar Milkov Gechev <92267207+PetyrGechev@users.noreply.github.com>
2022-01-23 17:06:18 +0000

    Update solution.js

--- SoftUni_JS/DOM-Introduction-Exercise-Resources-New/05. Table - Search Engine/solution.js
+++ SoftUni_JS/DOM-Introduction-Exercise-Resources-New/05. Table - Search Engine/solution.js
@@ -1,8 +1,19 @@
 function solve() {
    document.querySelector('#searchBtn').addEventListener('click', onClick);
-
+  
    function onClick() {
-      //   TODO:
+     let input=Array.from(document.querySelectorAll("tbody tr"));
+     let search=document.getElementById("searchField").value ;
+     input.forEach(element => {
+        
+        let row=(element.textContent);
+        if(row.includes(search)){
+         element.classList.add("select");
+        }else{
+           element.classList.remove("select");
+        }
+        
+     });
 
    }
 }
