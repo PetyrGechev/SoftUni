ef0dadf5824aca4e20497bada6412b8e4d6a8226
Petar Milkov Gechev <92267207+PetyrGechev@users.noreply.github.com>
2022-01-25 20:53:08 +0000

    Update app.js

--- SoftUni_JS/DOM-ManipulationAndEvents-Lab/04. Mouse-Gradient/app.js
+++ SoftUni_JS/DOM-ManipulationAndEvents-Lab/04. Mouse-Gradient/app.js
@@ -1,3 +1,14 @@
 function attachGradientEvents() {
-    console.log('TODO:...');
+    let result=document.getElementById("result");
+
+    let gradient=document.getElementById('gradient');
+    gradient.addEventListener("mousemove", solve);
+    function solve(ev){
+        
+        //gradient.removeEventListener("mousemove", solve);
+       let resultData=Number(Math.floor(ev.offsetX/ev.target.clientWidth*100))
+       console.log(resultData)
+       result.textContent=`${resultData}%`;
+    }
+
 }
