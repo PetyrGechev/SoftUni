ca9ace25ad3dcac7278a732935b7a26be9aade59
Petar Milkov Gechev <92267207+PetyrGechev@users.noreply.github.com>
2022-01-25 19:54:49 +0000

    Update app.js

--- SoftUni_JS/DOM-ManipulationAndEvents-Lab/03. Add-Delete/app.js
+++ SoftUni_JS/DOM-ManipulationAndEvents-Lab/03. Add-Delete/app.js
@@ -1,8 +1,20 @@
 function addItem() {
     let input=document.getElementById("newItemText").value ;
     let newLi=document.createElement("li");
+    let button=document.createElement('a');
+    button.href='#';
+    button.textContent='[Delete]';
+    
     newLi.textContent=input;
+    newLi.appendChild(button);
     let ud=document.getElementById("items");
     ud.appendChild(newLi);
-    console.log(input)
+    button.addEventListener('click', deleteItem);
+
+    function deleteItem(ev) {
+       ev.target.parentNode.parentNode.removeChild(ev.target.parentNode)
+
+    }
+  
+
 }
