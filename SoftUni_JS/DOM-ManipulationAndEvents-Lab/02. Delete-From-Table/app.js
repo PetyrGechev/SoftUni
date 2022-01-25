75612b648e930ad2332286d36949aa52213e8dfe
Petar Milkov Gechev <92267207+PetyrGechev@users.noreply.github.com>
2022-01-25 19:26:01 +0000

    Update app.js

--- SoftUni_JS/DOM-ManipulationAndEvents-Lab/02. Delete-From-Table/app.js
+++ SoftUni_JS/DOM-ManipulationAndEvents-Lab/02. Delete-From-Table/app.js
@@ -1,3 +1,17 @@
 function deleteByEmail() {
-    console.log('TODO:...');
+    let inputEmail=document.getElementsByName("email")[0].value;
+    let result=document.getElementById("result");
+    console.log(inputEmail)
+    let found=false;
+    let rows=Array.from(document.querySelectorAll("tbody tr td"));
+    rows.forEach(element => {
+       if(element.textContent==inputEmail){
+           console.log(element.parentNode.parentNode)
+           element.parentNode.parentNode.removeChild(element.parentNode);
+           found=true
+           
+       }
+
+    });
+    result.textContent = (found ) ? "Deleted." : "Not found.";
 }
