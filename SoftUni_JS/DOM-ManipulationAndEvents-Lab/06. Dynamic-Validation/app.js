2a71ab2cdfd417dee8b077ba337e788dbe7ffe2f
Petar Milkov Gechev <92267207+PetyrGechev@users.noreply.github.com>
2022-02-12 19:00:14 +0000

    exam prep

--- SoftUni_JS/DOM-ManipulationAndEvents-Lab/06. Dynamic-Validation/app.js
+++ SoftUni_JS/DOM-ManipulationAndEvents-Lab/06. Dynamic-Validation/app.js
@@ -1,14 +1,18 @@
-function validate() {
-    document.getElementById('email').addEventListener('change', onChange);
+// function validate() {
+//     document.getElementById('email').addEventListener('change', onChange);
 
-    function onChange(ev){
+//     function onChange(ev){
 
-        let pattern=/^[a-z]+@[a-z]+\.[a-z]+$/;
-        if(pattern.test(ev.target.value)){
-            ev.target.classList.remove('error')
-        }else{
-            ev.target.classList.add('error')
-        }
+//         let pattern=/^[a-z]+@[a-z]+\.[a-z]+$/;
+//         if(pattern.test(ev.target.value)){
+//             ev.target.classList.remove('error')
+//         }else{
+//             ev.target.classList.add('error')
+//         }
 
-    }
-}
+//     }
+// 
+const nested=document.createElement('tr'))
+let objs={src: "imags.bg"}
+nested[{src1: "imags.bg"}]={src: "imags.bg"};
+console.log(nested)
---
+++ SoftUni_JS/DOM-ManipulationAndEvents-Lab/06. Dynamic-Validation/test/test.js
@@ -0,0 +1,9 @@
+const sumOfTwoNum=require('./index');
+const assert=require('chai').assert;
+
+
+it('baba',()=>{
+    assert.equal(5,sumOfTwoNum(2,3))
+})
+
+console.log(sumOfTwoNum(2,3))
